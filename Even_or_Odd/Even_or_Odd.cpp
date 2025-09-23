df940c7a78ebdf5ed46c6b5b700b192354a0f611
Frozen-47 <sabareeshgm47@gmail.com>
2025-09-23 21:51:45 +0530

    First commit

---
+++ Even_or_Odd/Even_or_Odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int Sample_number;
+    bool isEven = false;
+    cout<<"Enter a Number to Find it (Even or Odd) :";
+    cin>>Sample_number;
+    if(Sample_number % 2 == 0)
+      isEven = true;
+    if(isEven == true){
+        cout<<Sample_number<<" is Even";
+    }
+    else{
+        cout<<Sample_number<<" is Odd";
+    }
+    return 0;
+}
