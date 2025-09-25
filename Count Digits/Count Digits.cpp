eaffc72332230433aee6870cab8a802007e002f8
Frozen-47 <sabareeshgm47@gmail.com>
2025-09-25 23:05:12 +0530

    Count Digits

---
+++ Count Digits/Count Digits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num , count = 0 , check;
+    cout<<"Enter the integer : ";
+    cin>>num;
+    int cpy = num;
+    while(cpy != 0){
+    check = cpy% 10;
+    if(check != 0 && num % check == 0){
+        count++;
+    }
+    cpy = cpy / 10;
+    }
+    cout<<count;
+}
