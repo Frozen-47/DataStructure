898502cbccd9daec5740ed0549f703e4637822f7
Frozen-47 <sabareeshgm47@gmail.com>
2025-10-29 20:02:16 +0530

    Sll Update

---
+++ DS/SLL/Singly Linked List.cpp
@@ -0,0 +1,17 @@
+//Insert and Delete at Begin....
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+}
+void insertAtBegin(){
+    
+}
+void deleteAtBegin(){
+    
+}
+int main(){
+
+    return 0;
+}
