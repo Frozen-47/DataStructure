fd3e6825c5e17d51b71a5b1465c3b9c8fd841760
Frozen-47 <sabareeshgm47@gmail.com>
2025-10-29 19:49:27 +0530

    Update Array

--- DS/Array/Array.cpp
+++ DS/Array/Array.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
 using namespace std;
+int main(){
+    int arr1[] = {1,2,3,4,5,6,7,8,9};
+    int arr2[sizeof(arr1) / sizeof(arr1[0])];
+    for(int i = 0 ;i < sizeof(arr1) / sizeof(arr1[0]);i++ ){
+        arr2[i] = arr1[i];
+    } 
+    for(int i = 0 ;i < sizeof(arr1) / sizeof(arr1[0]);i++ ){
+        cout << arr2[i] << " \n";
+    } 
+    return 0;
+}
