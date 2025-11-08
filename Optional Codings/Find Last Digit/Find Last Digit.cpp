987fae2b9787bc9033380cba448ba142d685adf7
Sabareesh G <sabareeshgm47@gmail.com>
2025-11-08 06:33:07 +0530

    Update Find Last Digit.cpp

--- Optional Codings/Find Last Digit/Find Last Digit.cpp
+++ Optional Codings/Find Last Digit/Find Last Digit.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int num,pow,result=1,last;
-    cout<<"Enter the Number and it's Power :";
+    cout<<"Enter the Number and it's Power ::";
     cin>>num>>pow;
     for(int i = 1;i <= pow;i++){
         result = result*num;
