3a5d976119bc099f8b29ac8d799fa1a4b56789fd
Frozen-47 <sabareeshgm47@gmail.com>
2025-11-21 07:01:47 +0530

    Update

--- DS/SLL/AtEnd.cpp
+++ DS/SLL/AtEnd.cpp
@@ -1,70 +1,91 @@
-//Insert and Delete at End....
-#include<iostream>
+// Insert and Delete at End....
+#include <iostream>
 using namespace std;
-struct Node{
+struct Node
+{
     int data;
     Node *next;
-    Node(int value){
+    Node(int value)
+    {
         data = value;
         next = NULL;
     }
 };
+class list
+{
+    public:
+    Node *head = NULL;
 
-Node *head = NULL;
-
-void insertAtEnd(int value){
-    Node *newNode = new Node(value);
-    if(head == NULL){
-       head = newNode;
-        return;
-    }
-    Node *temp = head;
-    while (temp->next != NULL){
-        temp = temp->next;
-    }
-    temp->next = newNode;
-
-    
-}
-void deleteAtEnd(){
-    if(head == NULL){
-        return;
-    }
-    Node *temp = head;
-   while (temp->next->next != NULL)
+    void insertAtEnd(int value)
     {
-        temp = temp->next;
+        Node *newNode = new Node(value);
+        if (head == NULL)
+        {
+            head = newNode;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
     }
-    delete temp->next;
-    
-}
-void print(){
-   if(head == NULL){
-    cout<<"Nothing is Here";
-        return;
-    }
-    Node *temp = head;
-    while (temp != NULL)
+    void deleteAtEnd()
     {
-        cout<<temp->data<<" -> ";
-        temp = temp->next;
+        if (head == NULL)
+        {
+            return;
+        }
+        if (head->next == NULL)
+        {
+            cout << "Deleted: " << head->data << endl;
+            delete head;
+            head = NULL;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = NULL;
     }
-    cout<<"NULL\n";
-}
-int main(){
-    int n,v;
-    cout<<"Enter the values : ";
-    cin>>n;
-    for(int i = 0 ; i <= n-1 ; i++){
-       cin>>v;
-       insertAtEnd(v);
+    void print()
+    {
+        if (head == NULL)
+        {
+            cout << "Nothing is Here";
+            return;
+        }
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
     }
-    print();
-    cout<<"How many node to Delete : ";
-    cin>>v;
-    for(int i = 0 ; i <= v-1 ; i++){
-       deleteAtEnd();
+};
+int main()
+{
+    list l;
+    int n, v;
+    cout << "Enter the values : ";
+    cin >> n;
+    for (int i = 0; i <= n - 1; i++)
+    {
+        cin >> v;
+        l.insertAtEnd(v);
     }
-    print();
+    l.print();
+    cout << "How many node to Delete : ";
+    cin >> v;
+    for (int i = 0; i < v; i++)
+    {
+        l.deleteAtEnd();
+    }
+    l.print();
     return 0;
 }
